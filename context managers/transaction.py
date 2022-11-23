99e057696a6914878c43941b675056336b313f8e
Sami Imran <samiimran2016@gmail.com>
2022-11-23 09:30:19 +0200

    more on context managers

---
+++ context managers/transaction.py
@@ -0,0 +1,15 @@
+from connection import Connection
+import contextlib
+
+
+class Transaction:
+    def __init__(self, conn):
+        self.conn = conn
+        self.xid = conn._start_transaction()
+
+    def commit(self):
+        self.conn._commit_transaction(self.xid)
+
+    def rollback(self):
+        self.conn._rollback_transaction(self.xid)
+
